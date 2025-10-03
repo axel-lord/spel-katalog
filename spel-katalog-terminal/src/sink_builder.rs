a759243fe48f3d5855905ce87c05518fcdfaa92e
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-10-03 22:53:59 +0200

    in spel-katalog-terminal, added lock function for SinkBuilder

--- spel-katalog-terminal/src/sink_builder.rs
+++ spel-katalog-terminal/src/sink_builder.rs
@@ -1,9 +1,9 @@
 //! [SinkBuilder] impl.
 
 use ::std::{
-    io::{PipeReader, pipe},
+    io::{PipeReader, PipeWriter, pipe},
     process::Stdio,
-    sync::mpsc::Sender,
+    sync::{Arc, mpsc::Sender},
 };
 
 use crate::SinkIdentity;
@@ -15,9 +15,39 @@ pub enum SinkBuilder {
     Inherit,
     /// Create and send a pipe.
     CreatePipe(Sender<(PipeReader, SinkIdentity)>),
+    /// Clone an already created pipe.
+    ClonePipe(Arc<PipeWriter>),
 }
 
 impl SinkBuilder {
+    /// Create a pipe ock the builder's ability to create more pipes.
+    /// Behaves differenctly depending on current state.
+    ///
+    /// If `CreatePipe` convert to `ClonePipe` with a newly created
+    /// pipe using the given identity.
+    ///
+    /// If `ClonePipe`, clone self.
+    ///
+    /// If `Inherit`, create a new `Inherit` builder.
+    pub fn with_locked_channel(
+        &self,
+        id: impl FnOnce() -> SinkIdentity,
+    ) -> ::std::io::Result<Self> {
+        Ok(match self {
+            SinkBuilder::Inherit => Self::Inherit,
+            SinkBuilder::CreatePipe(sender) => {
+                let (r, w) = pipe()?;
+
+                sender
+                    .send((r, id()))
+                    .map_err(|err| ::std::io::Error::other(err.to_string()))?;
+
+                Self::ClonePipe(Arc::new(w))
+            }
+            SinkBuilder::ClonePipe(pipe_writer) => Self::ClonePipe(pipe_writer.clone()),
+        })
+    }
+
     /// Get a process sink.
     pub fn build(&self, id: impl FnOnce() -> SinkIdentity) -> ::std::io::Result<Stdio> {
         match self {
@@ -31,6 +61,7 @@ impl SinkBuilder {
 
                 Ok(Stdio::from(w))
             }
+            SinkBuilder::ClonePipe(pipe_writer) => pipe_writer.try_clone().map(Stdio::from),
         }
     }
 
@@ -48,6 +79,10 @@ impl SinkBuilder {
 
                 Ok([w, w2].map(Stdio::from))
             }
+            SinkBuilder::ClonePipe(pipe_writer) => Ok([
+                pipe_writer.try_clone()?.into(),
+                pipe_writer.try_clone()?.into(),
+            ]),
         }
     }
 }
